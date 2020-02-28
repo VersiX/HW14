7ea045bc97cfac6a1f7b1f009e9b55be1425eaf8
VersiX <yurkatj7@gmail.com>
2020-02-28 23:40:03 +0200

    14.1

---
+++ 14.1/scripts/tabs.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("tab");
+    var dots = document.getElementsByClassName("tab-button");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" tab-button--active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " tab-button--active";
+    document.getElementById("image0").setAttribute("xlink:href", "img/avatar" + slideIndex + ".png");
+}
+
